42a35e2164ebbac445a3069fe9e22ebcf4961f37
Daemon <mfadlanf08@gmail.com>
2025-10-31 16:10:16 +0700

    Front End #1

--- frontend/src/components/Products/product.jsx
+++ frontend/src/components/Products/product.jsx
@@ -1,14 +1,18 @@
 export default function Product({ product }) {
-  console.log(product)
-  return (<>
-    <div className="bg-white px-4 py-4 rounded-2xl">
-      <div className="w-full h-32 overflow-hidden">
-        <img src={`http://localhost:8080${product.image_url}`} className="object-cover" alt="" />
-
-
+  console.log(product);
+  return (
+    <>
+      <div className="bg-white px-4 py-4 rounded-2xl">
+        <div className="w-full h-48 overflow-hidden">
+          <img
+            src={`http://localhost:8080${product.image_url}`}
+            className="object-cover"
+            alt=""
+          />
+        </div>
+        <p>{product.name}</p>
+        <p className="font-bold">Rp. {product.selling_price}</p>
       </div>
-      <p>{product.name}</p>
-      <p>{product.selling_price}</p>
-    </div>
-  </>)
+    </>
+  );
 }
--- frontend/src/components/Products/products.jsx
+++ frontend/src/components/Products/products.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from "react"
-import { getProducts } from "../../api/products"
-import Product from "./product"
+import React, { useEffect, useState } from "react";
+import { getProducts } from "../../api/products";
+import Product from "./product";
 
 export default function Products() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const data = await getProducts()
-        console.log(data)
-        setProducts(data)
+        const data = await getProducts();
+        console.log(data);
+        setProducts(data);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     }
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
-  return (<>
-    <div id="products">
-      <div className="w-2/3">
-        <div className="grid grid-cols-4 gap-x-4 gap-y-12">
-          {products.map(product => (
-            <Product product={product} key={product.id}></Product>
-          ))}
+  return (
+    <>
+      <div id="products">
+        <div className="w-2/3">
+          <div className="grid grid-cols-3 gap-x-4 gap-y-12">
+            {products.map((product) => (
+              <Product product={product} key={product.id}></Product>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-
-  </>)
+    </>
+  );
 }
