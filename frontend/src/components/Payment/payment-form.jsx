4c5ceba1f44892b92ce94c0cf263ccf545454f61
OtnielR <endidebora@gmail.com>
2025-11-05 15:33:13 +0700

    Form Payment Fix

--- frontend/src/components/Payment/payment-form.jsx
+++ frontend/src/components/Payment/payment-form.jsx
@@ -56,7 +56,7 @@ export default function PaymentForm({ productOrders, togglePaymentForm, handleTo
         <div className="bg-gray-300 py-2 px-4 rounded-lg">
           <div className="flex flex-row justify-between font-bold text-xl">
             <div className="">Total</div>
-            <div className="">{total}</div>
+            <div className="">{stringTotal}</div>
           </div>
         </div>
         <div>
